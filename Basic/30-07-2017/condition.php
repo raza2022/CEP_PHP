a9a0494251dd85d899a6151d552168cb1e2d1104
Abdul Hameed <raza2022@gmail.com>
2017-08-05 08:25:34 +0500

    both classes code added

---
+++ Basic/30-07-2017/condition.php
@@ -0,0 +1,55 @@
+<?PHP
+//condition 
+//if else
+// if(exp)
+// {
+
+// }
+// else
+// {
+
+// }
+$a = -10;
+// < > <= >= ! == && ||
+// if($a > 0)
+// {
+// 	echo "$a is greater than zero";
+// }
+// else
+// {
+// 	echo "$a is less than zero";
+// // }
+// $a = -12;
+// if($a > 0 && $a%2 == 0)
+// {
+// 	echo $a." is greter and an even number";
+// }
+// elseif($a > 0 && $a%2==1)
+// {
+// 	echo $a." is greter and an odd number";
+// }
+// elseif($a < 0 && $a%2==0)
+// {
+// 	echo $a." is less and an even number";
+// }
+// else
+// {
+// 	echo $a." is less and an odd number";
+// }
+
+// $a = 10;
+// $b = 
+
+// if(False && exp2 && exp3...exp10)
+// if(T || exp2 || exp3...exp10)
+// {
+
+// }
+$a = 10;
+$b = "10";
+if($a === $b)
+	echo "equal";
+else
+	echo "not equal";
+
+?>
---
+++ Basic/30-07-2017/variable.php
@@ -0,0 +1,56 @@
+<?PHP
+//echo "HELLO WORLD";
+//variables
+
+// $a = 20.5;
+// // echo $a;
+
+// echo gettype($a);
+// $b = "SSUET";
+// echo gettype($b);
+
+// $c = 'c';
+// echo gettype($c);
+//php has no char datatype
+
+// $year = "10";
+//echo gettype($a);
+//type casting 
+// $days = (int)$year * 365;
+// echo $days;
+
+//settype($year, int);
+
+// $a = "b";
+// $a = "I'm 10years";
+// $a;
+// echo intval($a);
+
+
+// checkbox , radio 
+
+// $a = 3.4;
+// echo intval($a);
+// echo round($a);
+
+
+// $no_of_products = 53;
+// $records_per_page = 10;
+// $no_of_pages = $no_of_products / $records_per_page;
+// echo ceil($no_of_pages);
+// echo "<br>".floor($no_of_pages);
+
+// $a = 10;
+// echo echo "variable a = $a";
+// print print '<br>variable a = '.$a;
+
+// $hello = "hi";
+// $hi = "Welcome";
+// echo $hello; // hi
+// echo $hi; //welcome
+// echo $$hello;
+
+
+
+
+?>
